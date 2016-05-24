bf8143ccff4f17ed17240368c255ec43c4587be6
zhenchaochen <charlie.chen.v@gmail.com>
2016-05-23 23:26:44 -0700

    add bootstrap and ng-bootstrap

--- client/src/app/components/pork/pork.ts
+++ client/src/app/components/pork/pork.ts
@@ -1,7 +1,34 @@
-import {Component} from '@angular/core';
+import {Component, ChangeDetectionStrategy} from '@angular/core';
+import {CORE_DIRECTIVES} from '@angular/common';
+import {TAB_DIRECTIVES} from 'ng2-bootstrap';
+
+// webpack html imports
+let template = require('./pork.html');
 
 @Component({
-    selector: 'app-home',
-    template: `<h2>Pork</h2>`
+    selector: 'tabs-demo',
+    directives: [TAB_DIRECTIVES, CORE_DIRECTIVES],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    template: template
 })
-export class Pork { }
+export class Pork {
+  public tabs:Array<any> = [
+    {title: 'Dynamic Title 1', content: 'Dynamic content 1'},
+    {title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true},
+    {title: 'Dynamic Title 3', content: 'Dynamic content 3', removable: true}
+  ];
+
+  public alertMe():void {
+    setTimeout(function ():void {
+      alert('You\'ve selected the alert tab!');
+    });
+  };
+
+  public setActiveTab(index:number):void {
+    this.tabs[index].active = true;
+  };
+
+  public removeTabHandler(/*tab:any*/):void {
+    console.log('Remove Tab handler');
+  };
+}
